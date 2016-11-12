70e3b16a03848e81f4176e06a28b84706dc889a7
skobrix <skobrix@gmail.com>
2016-11-12 14:22:10 -0800

    Home page change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,7 +75,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laras
                 </div>
 
                 <div class="links">
